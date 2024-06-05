e6ff1e8f1874f2a803c247c5f50f27eff30682c1
Krish-Lathidadiya <krishlathidadiya.sus@gmail.com>
2024-06-05 18:51:39 +0530

    Intialize All Files

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
+import Services from "./components/Services";
+import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
+import Footer from "./components/Footer";
+function App() {
+  return (
+    <>
+      <ButtonGradient />
+      <Header />
+      <Hero />
+      <Benefits />
+      <Collaboration />
+      <Services/>
+      <Pricing />
+      <Roadmap/>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/Tagline.jsx
@@ -0,0 +1,14 @@
+//it shows backet left and right []
+import brackets from "../assets/svg/Brackets";
+
+const TagLine = ({ className, children }) => {
+  return (
+    <div className={`tagline flex items-center ${className || ""}`}>
+      {brackets("left")} 
+      <div className="mx-3 text-n-3">{children}</div>
+      {brackets("right")}
+    </div>
+  );
+};
+
+export default TagLine;
